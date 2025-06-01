6b492c6b5bcbc3028a8e71b57d0beb058b24beff
Matteo Conti <matt.ciao@gmail.com>
2025-06-01 23:35:17 +0200

    improve layout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,8 +4,8 @@ import { LanguageToggle } from './language-toggle'
 
 export function Header() {
   return (
-    <header className="p-2 flex gap-2 bg-background text-primary justify-between">
-      <nav className="flex flex-row">
+    <header className="sticky top-0 z-50 w-full border-b bg-background">
+      <nav className="flex h-[var(--header-height)] items-center gap-2 px-4">
         <div className="px-2 font-bold">
           <Link to="/">Home</Link>
         </div>
@@ -13,7 +13,7 @@ export function Header() {
         <div className="px-2 font-bold">
           <Link to="/demo/tanstack-query">TanStack Query</Link>
         </div>
-        <div className='flex gap-2'>
+        <div className="flex gap-2">
           <ThemeToggle />
           <LanguageToggle />
         </div>

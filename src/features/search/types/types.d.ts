6abc43a23888ba4821d22ec01d168d0c1be10ef8
Matteo Conti <matt.ciao@gmail.com>
2025-06-04 20:41:24 +0200

    fix specimen types

--- src/features/search/types/types.d.ts
+++ src/features/search/types/types.d.ts
@@ -1,30 +1,35 @@
 type SpecimenData = {
   occurrenceID: string
-  catalogNumber: string
+  catalogNumber: string | null
   otherCatalogNumbers: string | null
   basisOfRecord: string | null
   verbatimIdentification: string | null
-  scientificName: string
-  verbatimEventDate: string
-  eventDate: string
-  year: number
-  month: number
-  locality: string
+  scientificName: string | null
+  verbatimEventDate: string | null
+  eventDate: string | null
+  year: number | null
+  month: number | null
+  locality: string | null
   verbatimElevation: string | null
   minimumElevationInMeters: number | null
   maximumElevationInMeters: number | null
-  country: string
+  country: string | null
   recordedBy: string | null
   identifiedBy: string | null
-  floritalyName: string
+  floritalyName: string | null
   wfoName: string | null
-  gbifName: string
+  gbifName: string  | null
   processedLocality: string | null
-  decimalLatitude: number
-  decimalLongitude: number
-  geodeticDatum: string
-  coordinatesUncertaintyInMeters: number
+  decimalLatitude: number | null
+  decimalLongitude: number | null
+  geodeticDatum: string | null
+  coordinatesUncertaintyInMeters: number | null
   georeferenceProtocol: string | null
+  multimedia: Array<MultimediaData>
 } & Record<string, unknown>
+
+type MultimediaData = {
+  identifier: string
+}
 
 export {SpecimenData}

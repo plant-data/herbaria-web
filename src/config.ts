2b9be1a7b8afc6ed19077d3218c13ac6326fc429
Matteo Conti <matt.ciao@gmail.com>
2025-06-04 14:26:04 +0200

    fix page being not a number

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 const baseApiUrl = import.meta.env.VITE_API_BASE_URL
 const baseImageUrl = import.meta.env.VITE_IMAGE_BASE_URL
-const ITEMS_PER_PAGE = import.meta.env.SPECIMENS_PER_PAGE
+const ITEMS_PER_PAGE = Number(import.meta.env.VITE_ITEMS_PER_PAGE)
 
 export { baseApiUrl, baseImageUrl, ITEMS_PER_PAGE }

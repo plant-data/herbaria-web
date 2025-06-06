4e1e034bd0882101d8d9b337ca1726c1a4462ea8
Matteo Conti <matt.ciao@gmail.com>
2025-06-06 19:20:16 +0200

    improve header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,6 +6,15 @@ export function Header() {
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background">
       <nav className="flex h-[var(--header-height)] items-center gap-2 px-4">
+        <div className="flex items-center gap-2">
+          <img
+            src="/images/flor.png"
+            alt="Herbaria Logo"
+            width={40}
+            height={40}
+            className="rounded-md"
+          />
+        </div>
         <div className="px-2 font-bold">
           <Link to="/">Home</Link>
         </div>

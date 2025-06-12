3c490f136ada7cb49dd7cb08aec447a0f1af713e
Matteo Conti <matt.ciao@gmail.com>
2025-06-13 01:04:27 +0200

    add link for external herbaria

--- src/routes/external/index.tsx
+++ src/routes/external/index.tsx
@@ -1,7 +1,32 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, redirect } from '@tanstack/react-router'
+import { useFilterStore } from '@/features/search/stores/use-filters-store'
 
+/**
+ * This route handles external links that redirect to the search page
+ * It allaws a small set of parameters to be passed in the URL
+ * Useful to link florItaly names to the search page
+ * Example usage:
+ * /external/?floritalyName=Olea europaea L.
+ */
 export const Route = createFileRoute('/external/')({
   component: RouteComponent,
+  validateSearch: (search) =>
+    search as {
+      floritalyName: string
+    },
+  loaderDeps: ({ search: { floritalyName } }) => ({
+    floritalyName,
+  }),
+  loader: ({ deps: { floritalyName } }) => {
+
+    if (floritalyName) {
+      useFilterStore.getState().setFloritalyName([floritalyName])
+      throw redirect({
+        to: '/search',
+      })
+    }
+    return {}
+  },
 })
 
 function RouteComponent() {

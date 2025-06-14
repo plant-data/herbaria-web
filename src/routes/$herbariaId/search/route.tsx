8411d90b0670b463dbd81442ceaf2ee92bd54538
Matteo Conti <matt.ciao@gmail.com>
2025-06-14 22:40:44 +0200

    improve filter reset logic and clean up code formatting

--- src/routes/$herbariaId/search/route.tsx
+++ src/routes/$herbariaId/search/route.tsx
@@ -1,7 +1,10 @@
-import  { useEffect } from 'react'
+import { useEffect } from 'react'
 import { Outlet, createFileRoute } from '@tanstack/react-router'
 import { useTranslation } from 'react-i18next'
-import type { FilterState, LockedFilters} from '@/features/search/stores/use-filters-store'
+import type {
+  FilterState,
+  LockedFilters,
+} from '@/features/search/stores/use-filters-store'
 import { SearchSidebar } from '@/features/search/components/search-sidebar'
 import {
   SidebarInset,
@@ -11,7 +14,6 @@ import {
 import { SpecimensNavbar } from '@/features/search/components/specimens-navbar'
 import { useFilterStore } from '@/features/search/stores/use-filters-store'
 import { useNavigationHistory } from '@/hooks/use-navigation-history' // Added import
-
 
 export const Route = createFileRoute('/$herbariaId/search')({
   component: RouteComponent,
@@ -25,27 +27,32 @@ export const Route = createFileRoute('/$herbariaId/search')({
 function RouteComponent() {
   const { t } = useTranslation()
   const resetFilters = useFilterStore(
-      (state: FilterState) => state.resetFilters,
-    )
-    const { previousRoute } = useNavigationHistory()
-  
-    useEffect(() => {
-      // necessario per resettare i filtri quando vengo da route esterne
-  
-      const shouldReset =
-        previousRoute &&
-        !previousRoute.startsWith('/search') &&
-        !previousRoute.startsWith('/specimens') &&
-        !previousRoute.startsWith('/external')
-  
-      if (shouldReset) {
-        console.log(
-          'Resetting filters because previous route was:',
-          previousRoute,
-        )
-        resetFilters()
-      }
-    }, [previousRoute, resetFilters])
+    (state: FilterState) => state.resetFilters,
+  )
+  const setInstitutionCode = useFilterStore(
+    (state: FilterState) => state.setInstitutionCode,
+  )
+  const { previousRoute } = useNavigationHistory()
+  const { herbariaId } = Route.useParams() // Get herbariaId from params
+
+  useEffect(() => {
+    // necessario per resettare i filtri quando vengo da route esterne
+
+    const shouldReset =
+      previousRoute &&
+      !previousRoute.startsWith(`/${herbariaId}/search`) && // Check for herbariaId
+      !previousRoute.startsWith(`/${herbariaId}/specimens`) && // Check for herbariaId
+      !previousRoute.startsWith(`/${herbariaId}/external`) // Check for herbariaId
+
+    if (shouldReset) {
+      console.log(
+        'Resetting filters because previous route was:',
+        previousRoute,
+      )
+      resetFilters()
+      setInstitutionCode([herbariaId])
+    }
+  }, [previousRoute, resetFilters, setInstitutionCode, herbariaId])
 
   const lockedFilters: LockedFilters = ['institutionCode']
 

6881f349a37723054b3f52a6fb92e3b35e0d90d7
Matteo Conti <matt.ciao@gmail.com>
2025-06-24 01:05:37 +0200

    add nav breadcrumb

---
+++ src/components/breadcrumb.tsx
@@ -0,0 +1,86 @@
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from '@/components/ui/breadcrumb'
+import { HERBARIA } from '@/features/search/constants/constants'
+import { Link, useRouterState, useParams } from '@tanstack/react-router'
+import { useTranslation } from 'react-i18next'
+
+export function BreadcrumbResponsive() {
+  const { location } = useRouterState()
+  const { herbariaId, occurrenceID } = useParams({ strict: false })
+  const { t } = useTranslation()
+  const pathnames = location.pathname.split('/').filter((x) => x)
+
+  const getHerbariumName = (id: string) => {
+    const herbarium = HERBARIA.find((h) => h.id === id)
+    console.log('herbarium', herbarium)
+    return herbarium ? t(herbarium.value) : id
+  }
+
+  const breadcrumbNameMap: { [key: string]: string | ((s: string) => string) } =
+  {
+    search: t('navbar.search'),
+    map: t('navbar.map'),
+    graphs: t('navbar.graphs'),
+    table: t('navbar.table'),
+    specimens: t('navbar.specimen'),
+  }
+
+  const getBreadcrumbName = (segment: string) => {
+    if (herbariaId && segment === herbariaId) {
+      return getHerbariumName(segment)
+    }
+    if (occurrenceID && segment === occurrenceID) {
+      return t('navbar.specimen') + ' ' + occurrenceID
+    }
+    const name = breadcrumbNameMap[segment]
+    if (typeof name === 'function') {
+      return name(segment)
+    }
+    return name || segment
+  }
+
+  if (pathnames.length === 0) {
+    return null
+  }
+  return (
+    <Breadcrumb>
+      <BreadcrumbList>
+        <BreadcrumbItem>
+          <BreadcrumbLink asChild>
+            <Link to="/">{t('navbar.home')}</Link>
+          </BreadcrumbLink>
+        </BreadcrumbItem>
+        {pathnames.map((value, index) => {
+          let to = `/${pathnames.slice(0, index + 1).join('/')}`
+          let isLast = index === pathnames.length - 1
+          let name = getBreadcrumbName(value)
+
+          if (value === 'specimens') {
+            // replace specimens with search
+            to = to.replace('specimens', 'search')
+            name = t('navbar.search')
+          }
+
+          return (
+            <BreadcrumbItem key={to}>
+              <BreadcrumbSeparator />
+              {isLast ? (
+                <BreadcrumbPage>{name}</BreadcrumbPage>
+              ) : (
+                <BreadcrumbLink asChild>
+                  <Link to={to}>{name}</Link>
+                </BreadcrumbLink>
+              )}
+            </BreadcrumbItem>
+          )
+        })}
+      </BreadcrumbList>
+    </Breadcrumb>
+  )
+} 

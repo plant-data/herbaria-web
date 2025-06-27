a385e62ee71cf214900f2daae5c222572202cb24
Matteo Conti <matt.ciao@gmail.com>
2025-06-27 17:44:46 +0200

    add images

---
+++ src/features/search/constants/herbaria.tsx
@@ -0,0 +1,29 @@
+export const HERBARIA_CONFIG = [
+  {
+    id: 'PI',
+    translationKey: 'herbaria.pi',
+    image: '/images/pi.png',
+    description:
+      "Explore the rich botanical heritage of the University of Pisa's herbarium collection",
+    badgeLabel: 'Herbarium PI',
+    homeImages: [
+      'https://object.jacq.org/europeana/PI/1310678.jpg',
+      'https://object.jacq.org/europeana/PI/2145582.jpg',
+      'https://object.jacq.org/europeana/PI/2204769.jpg',
+    ]
+  },
+  {
+    id: 'TSB',
+    translationKey: 'herbaria.tsb',
+    image: '/images/tsb.png',
+    description:
+      "Discover botanical specimens from the Trieste Science Museum's specialized collection",
+    badgeLabel: 'Herbarium TSB',
+    homeImages: [
+      'https://object.jacq.org/europeana/PI/1310678.jpg',
+      'https://object.jacq.org/europeana/PI/2145582.jpg',
+      'https://object.jacq.org/europeana/PI/2204769.jpg',
+    ]
+  },
+
+]

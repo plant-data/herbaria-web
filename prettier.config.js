d8bfeb96bd5a5a1e5ab00089015923c5a9d31ce5
Matteo Conti <matt.ciao@gmail.com>
2025-07-26 13:55:16 +0200

    change max line width

--- prettier.config.js
+++ prettier.config.js
@@ -3,6 +3,7 @@
 /** @type {import('prettier').Config} */
 const config = {
   semi: false,
+  printWidth: 100,
   singleQuote: true,
   trailingComma: 'all',
   plugins: ['prettier-plugin-tailwindcss'],

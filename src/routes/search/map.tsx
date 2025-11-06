686729de12f207b4df7d2483d95de436ea56fffe
Matteo Conti <matt.ciao@gmail.com>
2025-11-06 13:16:13 +0100

    fix import

--- src/routes/search/map.tsx
+++ src/routes/search/map.tsx
@@ -1,5 +1,5 @@
-import { SpecimensMap } from '@/features/search/components/specimens-map'
 import { createFileRoute } from '@tanstack/react-router'
+import { SpecimensMap } from '@/features/search/components/specimens-map'
 
 export const Route = createFileRoute('/search/map')({
   component: RouteComponent,

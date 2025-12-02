c7f32ea28c9324cdae037280828d338d99b6cee3
Matteo Conti <matt.ciao@gmail.com>
2025-12-02 11:42:45 +0100

    feat: add regions

---
+++ src/features/search/constants/regions.tsx
@@ -0,0 +1,27 @@
+const regions_array = [
+  'Abruzzo',
+  'Basilicata',
+  'Calabria',
+  'Campania',
+  'Emilia-Romagna',
+  'Friuli-Venezia Giulia',
+  'Lazio',
+  'Liguria',
+  'Lombardia',
+  'Marche',
+  'Molise',
+  'Piemonte',
+  'Puglia',
+  'Sardegna',
+  'Sicilia',
+  'Toscana',
+  'Trentino-Alto Adige',
+  'Umbria',
+  "Valle d'Aosta",
+  'Veneto',
+]
+
+export const regions: Array<{ id: string; value: string }> = regions_array.map((region) => ({
+  id: region,
+  value: region,
+}))
